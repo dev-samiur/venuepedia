8ce320ae300f26f310c3c3ad1619e3b4fdbc0ff0
Samiur Rahman <samiurrahman.dev@gmail.com>
2021-11-30 20:54:16 +0600

    chore: add hero component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,11 +3,11 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div>
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        
+        <Hero />
       </main>
     </div>
   )
