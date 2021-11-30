5278d0db0b1f97ec6f285603f914f61b85821a18
Samiur Rahman <samiurrahman.dev@gmail.com>
2021-11-30 20:56:36 +0600

    chore: add feature component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Hero from '../components/Hero'
+import Feature from '../components/Feature'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <Hero />
+				<Feature />
       </main>
     </div>
   )
