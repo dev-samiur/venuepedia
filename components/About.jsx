e5fc7b8fcec55c0d876068676c91ff812bda2313
Samiur Rahman <samiurrahman.dev@gmail.com>
2021-12-29 05:24:49 +0600

    chore: fix about page

--- components/About.jsx
+++ components/About.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const stats = [
   { label: 'Founded', value: '2021' },
-  { label: 'Employees', value: '5' },
+  { label: 'Employees', value: '150' },
   { label: 'Beta Users', value: '521' },
-  { label: 'Raised', value: '$25M' },
-]
+  { label: 'Raised', value: '$2M' },
+];
 
-const About= () => {
-	return (
+const About = () => {
+  return (
     <div className="relative bg-white py-16 sm:py-24">
       <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start">
         <div className="relative sm:py-16 lg:py-0">
-          <div aria-hidden="true" className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen">
+          <div
+            aria-hidden="true"
+            className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen"
+          >
             <div className="absolute inset-y-0 right-1/2 w-full bg-gray-50 rounded-r-3xl lg:right-72" />
             <svg
               className="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12"
@@ -30,10 +33,21 @@ const About= () => {
                   height={20}
                   patternUnits="userSpaceOnUse"
                 >
-                  <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
                 </pattern>
               </defs>
-              <rect width={404} height={392} fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)" />
+              <rect
+                width={404}
+                height={392}
+                fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)"
+              />
             </svg>
           </div>
           <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
@@ -47,13 +61,6 @@ const About= () => {
               <div className="absolute inset-0 bg-indigo-500 mix-blend-multiply" />
               <div className="absolute inset-0 bg-gradient-to-t from-indigo-600 via-indigo-600 opacity-90" />
               <div className="relative px-8">
-                <div>
-                  <img
-                    className="h-12"
-                    src="https://tailwindui.com/img/logos/workcation.svg?color=white"
-                    alt="Workcation"
-                  />
-                </div>
                 <blockquote className="mt-8">
                   <div className="relative text-lg font-medium text-white md:flex-grow">
                     <svg
@@ -65,13 +72,18 @@ const About= () => {
                       <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                     </svg>
                     <p className="relative">
-                      Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat, diam.
-                      Montes, magna cursus nulla feugiat dignissim id lobortis amet.
+                      Everything around us Bangladeshis are yet to experience
+                      the flexibility while selecting venues for their precious
+                      occasions. We aim to put you in charge of your event plan.
+                      With Venuepedia, your dream ceremony is just a few clicks
+                      away!"
                     </p>
                   </div>
 
                   <footer className="mt-4">
-                    <p className="text-base font-semibold text-indigo-200">Sarah Williams, CEO at Workcation</p>
+                    <p className="text-base font-semibold text-indigo-200">
+                      Sarah Williams, CEO at VenueQ
+                    </p>
                   </footer>
                 </blockquote>
               </div>
@@ -83,24 +95,27 @@ const About= () => {
           {/* Content area */}
           <div className="pt-12 sm:pt-16 lg:pt-20">
             <h2 className="text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl">
-              On a mission to empower teams
+              On a mission to empower people
             </h2>
             <div className="mt-6 text-gray-500 space-y-6">
               <p className="text-lg">
-                Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
-                pellentesque. Blandit amet, sed aenean erat arcu morbi. Cursus faucibus nunc nisl netus morbi vel
-                porttitor vitae ut. Amet vitae fames senectus vitae.
+                If we see the digitization of the world, the whole world is
+                moving towards the technological sector. Starting from
+                e-commerce, social media to even education is going to be
+                everything digital. Many of the companies help people to book a
+                car for their travel, rent a house, book a hotel to stay in the
+                vacation, etc.
               </p>
               <p className="text-base leading-7">
-                Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget
-                pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna. Sollicitudin tristique
-                eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu
-                viverra donec ut volutpat donec laoreet quam urna.
+                The VenueQ is a web application which helps the people to search
+                for a venue and reserve that according to their specific dates
+                and times.
               </p>
               <p className="text-base leading-7">
-                Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet
-                velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus
-                egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.
+                We have come up with a solution where the people will be able to
+                find any types of community centers or venues for their
+                different types of occasions like, birthday party, wedding, or
+                any office annual meetings.
               </p>
             </div>
           </div>
@@ -109,23 +124,24 @@ const About= () => {
           <div className="mt-10">
             <dl className="grid grid-cols-2 gap-x-4 gap-y-8">
               {stats.map((stat) => (
-                <div key={stat.label} className="border-t-2 border-gray-100 pt-6">
-                  <dt className="text-base font-medium text-gray-500">{stat.label}</dt>
-                  <dd className="text-3xl font-extrabold tracking-tight text-gray-900">{stat.value}</dd>
+                <div
+                  key={stat.label}
+                  className="border-t-2 border-gray-100 pt-6"
+                >
+                  <dt className="text-base font-medium text-gray-500">
+                    {stat.label}
+                  </dt>
+                  <dd className="text-3xl font-extrabold tracking-tight text-gray-900">
+                    {stat.value}
+                  </dd>
                 </div>
               ))}
             </dl>
-            <div className="mt-10">
-              <a href="#" className="text-base font-medium text-indigo-600">
-                {' '}
-                Learn more about how we're changing the world <span aria-hidden="true">&rarr;</span>{' '}
-              </a>
-            </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
